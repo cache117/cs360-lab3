15c606cf533be81599092fdeb257d3042092c697
Cache Staheli <cache@byu.edu>
2016-02-01 18:33:34 +0000

    threads

--- thread.cpp
+++ thread.cpp
@@ -1,6 +1,24 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <queue>
+#include <iostream>
+
+class ThreadQueue
+{
+	std:queue<int> stlqueue;
+	public:
+	void push(int sock)
+	{
+		stlqueue.push(sock);
+	}
+	int pop()
+	{
+		int rval = stlqueue.front();
+		stlqueue.pop();
+		return rval;
+	}
+} sockqueue;
 
 void *howdy(void *arg)
 {
@@ -15,10 +33,19 @@ void *howdy(void *arg)
 }
 int main()
 {
-#define NTHREADS 20
+#define NTHREADS 10
+#define NQUEUE	 20
 	long threadid;
 	pthread_t threads[NTHREADS];
-
+	
+	for (int i = 0; i < NTHREADS; i++)
+	{
+		sockqueue.push(i);
+	}
+	for(int i = 0; i < NTHREADS; i++)
+	{
+		std::cout << "GOT" << sockqueue.pop() << std::endl;
+	}
 	for(threadid = 0; threadid < NTHREADS; threadid++)
 	{
 		pthread_create(&threads[threadid], NULL, howdy, (void *) threadid);
